1803ce80b26c49170417284776bc5113d9cc5f0c
Pedro_hlc <pedro1902lisardo@gmail.com>
2026-09-22 20:49:25 -0300

    22/09/2026 atividade agora certo 1

---
+++ src/Saudacao.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+function Saudacao({nome}){
+    return(
+        <div className='Saudacao'>
+            <h1> Olá, {nome}</h1>
+        </div>
+    )
+}
+export default Saudacao;
